e0d26bcff2e24665dbeffd3de80a355c69309dc2
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-02-03 22:51:59 -0800

    creating feed item module

---
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -0,0 +1,17 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Hiram Castro on 03/02/22.
+//
+
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
