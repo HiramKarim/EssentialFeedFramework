f2286fb76f532070a978f3f3669b313b146aa520
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-02-16 21:29:09 -0800

    make http protocol

--- EssentialFeedModule/EssentialFeedModuleTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedModule/EssentialFeedModuleTests/RemoteFeedLoaderTests.swift
@@ -8,18 +8,43 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
         
         XCTAssertNil(client.requestedURL)
     }
