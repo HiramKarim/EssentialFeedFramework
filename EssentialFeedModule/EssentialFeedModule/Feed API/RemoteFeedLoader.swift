aec9cf0e110509d30618b31079403d5131bd3937
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-04-13 22:04:35 -0700

    Moved mapper function to FeedItemMapper

--- EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
+++ EssentialFeedModule/EssentialFeedModule/Feed API/RemoteFeedLoader.swift
@@ -30,19 +30,11 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                completion(self.map(data, from: response))
+                completion(FeedItemsMapper.map(data, from: response))
             case .failure:
                 completion(.failure(.connectivity))
             }
         }
     }
     
-    private func map(_ data: Data, from response: HTTPURLResponse) -> Result {
-        do {
-            let items = try FeedItemsMapper.map(data, response)
-            return .success(items)
-        } catch {
-            return .failure(.invalidData)
-        }
-    }
 }
