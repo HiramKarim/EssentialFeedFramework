7186a9bba1133549a80c805ed6cf9f7b6db67b09
Hiram Castro <hiramcastrodeveloper@gmail.com>
2022-08-08 23:15:44 -0700

    moving url creation to factory method

--- EssentialFeedModule/EssentialFeedModuleTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedModule/EssentialFeedModuleTests/URLSessionHTTPClientTests.swift
@@ -49,7 +49,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performsGETRequestWithURL() {
         
-        let url = URL(string: "http:any-url.com")!
+        let url = makeURL()
         let exp = expectation(description: "wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -64,7 +64,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http:any-url.com")!
+        let url = makeURL()
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
                 
@@ -90,6 +90,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func makeURL() -> URL {
+        return URL(string: "http:any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
